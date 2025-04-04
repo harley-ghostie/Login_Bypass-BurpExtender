9de3ec01113f846dd3afa36614e1738ae90f00b5
harley-ghostie <harleyvpn99@proton.me>
2025-04-04 14:47:01 -0300

    Create CustomAuditIssue.java

---
+++ src/loginbypass/CustomAuditIssue.java
@@ -0,0 +1,113 @@
+package loginbypass;
+
+import burp.api.montoya.collaborator.Interaction;
+import burp.api.montoya.http.HttpService;
+import burp.api.montoya.http.message.HttpRequestResponse;
+import burp.api.montoya.scanner.audit.issues.AuditIssue;
+import burp.api.montoya.scanner.audit.issues.AuditIssueConfidence;
+import burp.api.montoya.scanner.audit.issues.AuditIssueDefinition;
+import burp.api.montoya.scanner.audit.issues.AuditIssueSeverity;
+
+import java.util.Collections;
+import java.util.List;
+
+public class CustomAuditIssue implements AuditIssue {
+    private final String name;
+    private final String baseUrl;
+    private final String detail;
+    private final String remediation;
+    private final List<HttpRequestResponse> httpMessages;
+    private final AuditIssueSeverity severity;
+    private final AuditIssueConfidence confidence;
+
+    public CustomAuditIssue(String name,
+                             String baseUrl,
+                             String detail,
+                             String remediation,
+                             List<HttpRequestResponse> httpMessages,
+                             AuditIssueSeverity severity,
+                             AuditIssueConfidence confidence) {
+        this.name = name;
+        this.baseUrl = baseUrl;
+        this.detail = detail;
+        this.remediation = remediation;
+        this.httpMessages = httpMessages;
+        this.severity = severity;
+        this.confidence = confidence;
+    }
+
+    @Override
+    public String name() {
+        return name;
+    }
+
+    @Override
+    public AuditIssueDefinition definition() {
+        return new AuditIssueDefinition() {
+            @Override
+            public String name() {
+                return name;
+            }
+
+            @Override
+            public String background() {
+                return "Detectado código JavaScript indicando controle de login no lado cliente.";
+            }
+
+            @Override
+            public String remediation() {
+                return remediation;
+            }
+
+            @Override
+            public int typeIndex() {
+                return 0;
+            }
+
+            @Override
+            public AuditIssueSeverity typicalSeverity() {
+                return severity;
+            }
+        };
+    }
+
+    @Override
+    public String baseUrl() {
+        return baseUrl;
+    }
+
+    @Override
+    public String detail() {
+        return detail;
+    }
+
+    @Override
+    public String remediation() {
+        return remediation;
+    }
+
+    @Override
+    public List<Interaction> collaboratorInteractions() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public AuditIssueSeverity severity() {
+        return severity;
+    }
+
+    @Override
+    public AuditIssueConfidence confidence() {
+        return confidence;
+    }
+
+    @Override
+    public List<HttpRequestResponse> requestResponses() {
+        return httpMessages;
+    }
+
+    @Override
+    public HttpService httpService() {
+        return httpMessages.get(0).request().httpService();
+    }
+} 
